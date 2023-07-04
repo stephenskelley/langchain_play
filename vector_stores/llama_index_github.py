850098e61b1f7bc2e33a9c981e76521c8ea41e13
Stephen <stevek@virtustate.com>
2023-07-04 18:51:34 -0500

    pinecone working

--- vector_stores/llama_index_github.py
+++ vector_stores/llama_index_github.py
@@ -1,6 +1,7 @@
 import pickle
 import os
-from llama_index import OpenAIEmbedding, ServiceContext, download_loader, GPTVectorStoreIndex, StorageContext
+from llama_index import ServiceContext, download_loader, GPTVectorStoreIndex, StorageContext, OpenAIEmbedding
+# from langchain.embeddings.openai import OpenAIEmbeddings
 from llama_index.vector_stores.qdrant import QdrantVectorStore
 from llama_index.vector_stores.pinecone import PineconeVectorStore
 from llama_index.node_parser.simple import SimpleNodeParser
@@ -42,6 +43,7 @@ if docs is None:
         pickle.dump(docs, f)
 
 embed_model = OpenAIEmbedding()
+embed_model.openai_kwargs = {'api_key': secrets.techstyle_openai_key}
 # https://github.com/jerryjliu/llama_index/issues/1206 - handle special tokens like <|endoftext|>
 node_parser = SimpleNodeParser(text_splitter=TokenTextSplitter(allowed_special={"<|endoftext|>"}))
 service_context = ServiceContext.from_defaults(embed_model=embed_model, node_parser=node_parser)
@@ -52,7 +54,7 @@ service_context = ServiceContext.from_defaults(embed_model=embed_model, node_par
 
 #pinecone
 client = pinecone.init(api_key=secrets.techstyle_pinecone_api_key, environment='us-east-1-aws')
-vector_store=PineconeVectorStore(client=client, index_name='ssk', collection_name='github-llama-index', environment='us-east-1-aws')
+vector_store=PineconeVectorStore(client=client, index_name='ssk', namespace='github-llama-index', environment='us-east-1-aws')
 
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 index = GPTVectorStoreIndex.from_documents(docs, storage_context=storage_context, service_context=service_context)
